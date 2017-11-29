299d23f854d0e47c035424790adbd966e7502a13
Tagma-1 <31788490+tagma-1@users.noreply.github.com>
2017-11-30 10:37:03 +1100

    Move quote data to state

--- src/App.js
+++ src/App.js
@@ -4,18 +4,26 @@ import './App.css';
 import StockInfo from './components/Stockinfo'
 
 class App extends Component {
+  state={
+    quote: {
+      symbol: 'NFLX',
+      companyName: 'Netflix Inc.',
+      primaryExchange: 'Nasdaq Global Select',
+      latestPrice: 188.15,
+      latestSource: 'Close',
+      week52High: 204.38,
+      week52Low: 113.95
+    }
+  }
+
   render() {
+    const { quote } = this.state; 
+
     return (
       <div className="App">
         <h1>Stock Price App</h1>  
         <StockInfo 
-          symbol='NFLX'
-          companyName='Netflix Inc.'
-          primaryExchange='Nasdaq Global Select'
-          latestPrice={ 188.15 }
-          latestSource='Close'
-          week52High={ 204.38 }
-          week52Low={ 113.95 }
+          { ...quote }
         />
       </div>
     );
