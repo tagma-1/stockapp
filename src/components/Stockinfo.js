754ab556d75a7ca334f762384e5296a1bb4b9930
Tagma-1 <31788490+tagma-1@users.noreply.github.com>
2017-11-30 21:07:05 +1100

    Adjust price chart domain on y-axis

--- src/components/Stockinfo.js
+++ src/components/Stockinfo.js
@@ -47,7 +47,7 @@ function StockInfo({
       margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
         <CartesianGrid strokeDasharray="3 3" />
         <XAxis dataKey="date" />
-        <YAxis dataKey="close" />
+        <YAxis dataKey="close" domain={["dataMin - 10", "dataMax + 10"]} />
         <Tooltip />
         <Legend />
         {/* <Line type="monotone" dataKey="close" stroke="#8884d8" /> */}
